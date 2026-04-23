b639f51548e9439f44d7eac8871792d37e3c660f
AVINASH-SS <avinashsathyakumar@gmail.com>
2026-04-23 20:33:24 +0530

    Day 1: Basic Math Problems
    Solved basic math-based problems from Striver DSA Sheet.
    Covered concepts like:
    - Prime number check
    - Armstrong number
    - GCD using Euclidean algorithm
    - Palindrome check
    - Reverse number
    - Divisor printing

---
+++ GCD_of_Two_Numbers.java
@@ -0,0 +1,13 @@
+public class GCD_of_Two_Numbers {
+    public static void main(String args[]){
+        int a=48;
+        int b=18;
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        System.out.print(a);
+        //eucilidean algortithmal
+    }
+}
---
+++ Palindrome_Number.java
@@ -0,0 +1,22 @@
+public class Palindrome_Number {
+    public static void main(String args[]){
+        int num=121;
+        Palindrome_Number obj=new Palindrome_Number();
+        obj.reverse(num);
+    }
+    public void reverse(int num){
+         int r=0;
+         int c=num;
+        while(num!=0) {
+            r = r * 10 + (num % 10);
+            num = num / 10;
+        }
+         if(r==c){
+             System.out.print("Palindrome");
+         }
+         else{
+             System.out.print("Not a palindrome");
+         }
+
+    }
+}
