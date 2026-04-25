5686c96bb5a89da63961f559de9a3cb4137bcd4b
AVINASH-SS <avinashsathyakumar@gmail.com>
2026-04-25 19:40:36 +0530

    Day3:Sorting technique

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -10,7 +10,7 @@ public class Bubble_sort {
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+                    arr[j+1]=temp; 
                 }
             }
         }
