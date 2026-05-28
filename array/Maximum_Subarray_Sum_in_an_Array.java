63d552f8cc49d46b7f2b4a8229b864a9fb5fa600
AvinashSS-03 <avinashssathyakumar@gmail.com>
2026-05-28 23:39:17 +0530

    Day 9:Subarray

--- array/Maximum_Subarray_Sum_in_an_Array.java
+++ array/Maximum_Subarray_Sum_in_an_Array.java
@@ -3,16 +3,16 @@ package array;
 public class Maximum_Subarray_Sum_in_an_Array {
     public static void main(String[] args) {
         int[] arr={2, 3, 5, -2, 7, -4};
-        int max=Integer.MIN_VALUE;
+        int Max=Integer.MIN_VALUE;
 
         for(int i=0;i<arr.length;i++){
             int sum=0;
             for(int j=i;j<arr.length;j++){
                 sum+=arr[j];
-                max=Math.max(max,sum);
+                Max=Math.max(Max,sum);
             }
 
         }
-        System.out.print(max);
+        System.out.print(Max);
     }
 }
