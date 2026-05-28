20c069cd585ddbbdc9bd8d132c622f7e43f3dc97
AvinashSS-03 <avinashssathyakumar@gmail.com>
2026-05-28 23:55:32 +0530

    Day 9:Subarray

--- array/Maximum_Subarray_Sum_in_an_Array.java
+++ array/Maximum_Subarray_Sum_in_an_Array.java
@@ -1,5 +1,8 @@
 package array;
-
+/*
+Problem Statement:we have to return the maximum subarray sum in an array
+Statement:we can use brute forve approach and sum all the subarrays and find the maximum sum in a subarray
+ */
 public class Maximum_Subarray_Sum_in_an_Array {
     public static void main(String[] args) {
         int[] arr={2, 3, 5, -2, 7, -4};
@@ -9,7 +12,7 @@ public class Maximum_Subarray_Sum_in_an_Array {
             int sum=0;
             for(int j=i;j<arr.length;j++){
                 sum+=arr[j];
-                Max=Math.max(Max,sum);
+                Max=Math.max(Max,sum); //checking the maximum sum
             }
 
         }
