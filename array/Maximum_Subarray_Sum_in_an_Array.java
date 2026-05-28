d695d9db9164e791515fc194e0452fd7fc843491
AvinashSS-03 <avinashssathyakumar@gmail.com>
2026-05-28 23:37:32 +0530

    Day 9:Subarray

---
+++ array/Maximum_Subarray_Sum_in_an_Array.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Maximum_Subarray_Sum_in_an_Array {
+    public static void main(String[] args) {
+        int[] arr={2, 3, 5, -2, 7, -4};
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                max=Math.max(max,sum);
+            }
+
+        }
+        System.out.print(max);
+    }
+}
