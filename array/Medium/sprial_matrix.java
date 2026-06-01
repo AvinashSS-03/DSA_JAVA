513730191423c4a2cb2ba2f21a7e341669816164
AvinashSS-03 <avinashssathyakumar@gmail.com>
2026-06-01 21:48:56 +0530

    Array Medium problems

--- array/Medium/sprial_matrix.java
+++ array/Medium/sprial_matrix.java
@@ -6,30 +6,30 @@ Solution:We can travese from left to right from top to bottom and create a list 
  */
 public class sprial_matrix {
     public static void main(String[] args) {
-        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] smatrix={{1,2,3},{4,5,6},{7,8,9}};
         int top=0; //to keep count of top row
-        int bottom=matrix.length-1; //to keep count of bottom row
+        int bottom=smatrix.length-1; //to keep count of bottom row
         int left=0; //to keep left element
-        int right=matrix[0].length-1; //to keep right element
+        int right=smatrix[0].length-1; //to keep right element
         List<Integer> result=new ArrayList<>();//creating an array list to store
         while(top<=bottom && left<=right){ //either top <= bottom and left<=right
             for(int i=left;i<=right;i++){
-                result.add(matrix[top][i]); //add first row
+                result.add(smatrix[top][i]); //add first row
             }
             top++;//increase to next row because first row is added
             for(int i=top;i<=bottom;i++){
-                result.add(matrix[i][right]);   //add right last columns
+                result.add(smatrix[i][right]);   //add right last columns
             }
             right--; //decrease the right because we have added right column
             if(top<=bottom){
                 for(int i=right;i>=left;i--){
-                    result.add(matrix[bottom][i]);//to add botttom row from n-1;
+                    result.add(smatrix[bottom][i]);//to add botttom row from n-1;
                 }
                 bottom--;
             }
             if(left<=right){
                 for(int i=bottom;i>=top;i--){
-                    result.add(matrix[i][left]); //from left to right
+                    result.add(smatrix[i][left]); //from left to right
                 }
                 left++;
             }
